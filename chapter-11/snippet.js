1c8efce793c47de21a5e0f37096eeb33bfc98617
Jos Dirksen <jos@jpoint.nl>
2015-02-19 20:14:34 +0100

    Another set of updates

---
+++ chapter-11/snippet.js
@@ -0,0 +1,8 @@
+var scene = new THREE.Scene();
+
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setClearColorHex()
+renderer.setClearColor(new THREE.Color(0xEEEEEE));
+renderer.setSize(window.innerWidth, window.innerHeight);
